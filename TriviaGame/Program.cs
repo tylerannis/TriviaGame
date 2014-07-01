d3133aa312979d57f3514f5afa3bd27bd16b4f74
Dustin Kraft <kraftdr@gmail.com>
2014-07-01 16:26:36 -0600

    Changes

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -11,16 +11,26 @@ namespace TriviaGame
     {
         static void Main(string[] args)
         {
+            //Trivia game function goes here
+
         }
 
+
+        //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
             //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
             List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
 
             //Each item in list "contents" is now one line of the Trivia.txt document.
-
-            return new List<Trivia>();
+            
+            //make a new list to return all trivia questions
+            List<Trivia> returnList = new List<Trivia>();
+            // TODO: go through each line in contents of the trivia file and make a trivia object.
+            //       add it to our return list.
+            
+            //Return the full list of trivia questions
+            return returnList;
         }
     }
 }
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -8,6 +8,7 @@ namespace TriviaGame
 {
     class Trivia
     {
-     
+        //TODO: Fill out the Trivia Object
+        
     }
 }
