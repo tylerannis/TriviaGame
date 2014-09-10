0686835c963aca9df1fb9690c741e02e01c45194
SeedPaths-User <lessdismissive@email.com>
2014-09-10 11:59:11 -0600

    Trivia Game1

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -8,11 +8,67 @@ using System.Threading.Tasks;
 namespace TriviaGame
 {
     class Program
-    {
+    {//The logic for your trivia game happens here
+        static List<Trivia> AllQuestions;
         static void Main(string[] args)
         {
             //The logic for your trivia game happens here
-            List<Trivia> AllQuestions = GetTriviaList();
+            AllQuestions = GetTriviaList();
+            //greet the user and ask for their name
+            Console.WriteLine("Hello and welcome to the almost decent trivia game! What is your name?");
+            string name = Console.ReadLine();
+            //print new greeting and rules with name
+            Console.WriteLine("Hey there " + name + ". We are going to ask you a few questions and if you get them right you could earn up to 5000 useless points!\nOn the other hand... if you get just five questions wrong, you will lose and your name will not go down in history.");
+            Console.WriteLine("Let's play Who's A Big Fat Nerd!\n");
+            //set variables for number of wrong guesses
+            int lives = 5;
+            int score = 0;
+           
+           
+            //set up something to randomly loop through the triva list and give a question while lives is > 0
+            while (lives >= 0 || score <= 5000)
+            {
+                int currentscore = 0;
+                //make a random number generator
+                Random newQuestion = new Random();
+                int number = newQuestion.Next(0, AllQuestions.Count());
+                Trivia CurrentQues = AllQuestions[number];
+                //return question
+                Console.WriteLine(CurrentQues.AskQuestion());
+                //tell them to write an answer
+                Console.WriteLine("Type your answer");
+              string answer = Console.ReadLine().ToLower();
+              if (answer == CurrentQues.GiveAnswer())
+              {
+                  currentscore++;
+                  Console.WriteLine("[Your Score: {0}]", currentscore + "\n[Your Lives: {1}]", lives);
+                  Console.WriteLine("\n\n\nGreat job! I can't believe you knew that! Your get a point!");
+              }
+              else
+              {
+                  lives--;
+                  Console.WriteLine("[YourScore: {0}]", currentscore + "\n[Your Lives: {1}]", lives);
+                  Console.WriteLine("\n\n\nYou lose a life");
+                  Console.WriteLine("\nThe answer is {0}", CurrentQues.GiveAnswer());
+                  if (lives == 4)
+                  {
+                      Console.WriteLine("\nYou are so dumb, I can't believe you missed that. You lose a life.\n");
+                  }
+                  else if (lives == 3)
+                  {
+                      Console.WriteLine("\nWhat the hell is wrong with you! Didn't you go to grade school! You idiot!\n");
+                  }
+                  else if (lives == 2)
+                  {
+                      Console.WriteLine("\nOh my god you are totally off the Jepordy team. I didn't know anyone could be this dumb.\n");
+                  }
+                  else if (lives == 1)
+                  {
+                      Console.WriteLine("\nJust give up you suck...\n");
+                  }
+              }
+
+            }
         }
 
 
@@ -26,6 +82,10 @@ namespace TriviaGame
             
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
+            foreach (var item in contents)
+            {
+                returnList.Add(new Trivia(item));
+            }
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -13,12 +13,40 @@ namespace TriviaGame
         //The Trivia Object will have 2 properties
         // at a minimum, Question and Answer
 
+        //set your properties
+        public string Question { get; set; }
+        public string Answer { get; set; }
         //The Constructor for the Trivia object will
         // accept only 1 string parameter.  You will
         // split the question from the answer in your
         // constructor and assign them to the Question
         // and Answer properties
 
+        //Set the Class Trivia to a string
+        public Trivia(string splitAnswer)
+        {
+            //You need something to hold both the question and answer once you split them
+            List<string> hold = new List<string>();
+            //split Trivia string now both question and answer can be seperate strings with seperate indexs
+            hold = splitAnswer.Split('*').ToList();
+
+
+            //setting question to hold the value of the string before the *
+            //and setting answer to hold the value of the string after the *
+           
+            this.Question = hold[0].ToString();
+            this.Answer = hold[1].ToString();
+        }
+        //Methods and Functions
+        //this is a function to ask a question
+        public string AskQuestion()
+        {
+            return this.Question;
+        }
+        public string GiveAnswer()
+        {
+            return this.Answer.ToLower();
+        }
 
     }
 }
